e66b48648a0484fb111e908ec7cd59df275fe27c
Carl Hörberg <carl.hoerberg@gmail.com>
2020-03-15 16:09:57 +0100

    wip: extract properties

--- src/rmqrecover.cr
+++ src/rmqrecover.cr
@@ -7,6 +7,7 @@ class RMQRecover
   record Message,
     exchange : String,
     routing_key : String,
+    properties : AMQP::Client::Properties,
     body : IO::Memory
 
   EXCHANGE = "exchange"
@@ -45,8 +46,6 @@ class RMQRecover
       end
       puts "Found #{i} messages in vhost #{vhost}"
     end
-  rescue ex
-    abort ex.message
   end
 
   # finds vhost directories in a directy
@@ -80,33 +79,144 @@ class RMQRecover
     message_files(path) do |file|
       File.open(file) do |f|
         f.buffer_size = 1024 * 1024
-        extract f, &blk
+        STDERR.puts "Extracting from #{file}"
+        hex = IO::Hexdump.new(f, read: true)
+        extract hex, File.extname(file), &blk
       end
     end
   end
 
   # Yields messages from a rabbitmq message file
   # currently only reads exchange, routing key and body
-  # TODO: also parse properties (including headers)
-  private def extract(io, &blk : Message -> Nil)
+  # erlang binary format description:
+  # http://erlang.org/doc/apps/erts/erl_ext_dist.html
+  private def extract(io, ext, &blk : Message -> Nil)
     body = IO::Memory.new
     loop do
-      skip_until(io, EXCHANGE)
-      io.skip 1
-      exchange_len = io.read_bytes Int32, IO::ByteFormat::NetworkEndian
-      exchange = io.read_string exchange_len
-      io.skip 6
-      rk_len = io.read_bytes Int32, IO::ByteFormat::NetworkEndian
-      rk = io.read_string rk_len
-      skip_until(io, MESSAGE)
+      case ext
+      when ".idx"
+        io.skip 37 # in idx files
+      when ".rdq"
+        io.read_bytes Int32, IO::ByteFormat::NetworkEndian # 0x00000000
+        size = io.read_bytes Int32, IO::ByteFormat::NetworkEndian
+        io.skip 18 # ?
+      end
+
+      value(io) # basic_message
+
+      io.read_byte # 0x68 small tuple
+      io.read_byte # 0x04 small tuple items
+
+      value(io) # "resource"
+      vhost = value(io)
+      value(io) # "exchange"
+      exchange = value(io).as(String)
+
+      io.read_byte # 0x6c (list)
+      _ = io.read_bytes Int32, IO::ByteFormat::NetworkEndian # 0001 list items
+
+      rk = value(io).as(String)
+      value(io) # nil
+      value(io) # "content"
+      value(io) # 0x3c (60) some small int?
+
+      io.read_byte # 0x68 small tuple
+      io.read_byte # 0x0f small tuple items
+
+      value(io) # "P_basic"
+
+      p = AMQP::Client::Properties.new
+
+      p.content_type = value(io).as?(String)
+      p.content_encoding = value(io).as?(String)
+
+      # headers
+      case io.read_byte
+      when 0x6a # empty table
+        io.skip 2 # 0x61 0x02
+      when 0x6c # table
+        headers_len = io.read_bytes Int32, IO::ByteFormat::NetworkEndian
+
+        headers = AMQ::Protocol::Table.new
+        headers_len.times do
+          io.read_byte # 0x68 small tuple
+          io.read_byte # 0x03 tuple items
+
+          key = value(io).as(String)
+          value_type = value(io).as(String)
+
+          value =
+            case value_type
+            when "bool"
+              io.read_string(io.read_bytes Int16, IO::ByteFormat::NetworkEndian) == "true"
+            when "long"
+              io.skip 2
+              io.read_bytes Int16, IO::ByteFormat::NetworkEndian
+            when "longstr"
+              io.read_string(io.read_bytes Int32, IO::ByteFormat::NetworkEndian)
+            end
+        end
+        p.headers = headers
+      end
+
+      if priority = value(io).as?(Int)
+        p.priority = priority > UInt8::MAX ? UInt8::MAX : priority.to_u8
+      end
+
+      p.correlation_id = value(io).as?(String)
+      p.reply_to = value(io).as?(String)
+      p.expiration = value(io).as?(String)
+      p.message_id = value(io).as?(String)
+      if timestamp = value(io).as?(Int)
+        p.timestamp = Time.unix timestamp
+      end
+      p.type = value(io).as?(String)
+      p.user_id = value(io).as?(String)
+      p.app_id = value(io).as?(String)
+      value(io) # reserved1/cluster_id
+      value(io) # none?
+      value(io) # none?
+
+      io.read_byte # 0x6c list
+      io.skip 4 # 0x00000001 list items
+      io.read_byte # 0x6d long string
       body_len = io.read_bytes Int32, IO::ByteFormat::NetworkEndian
       IO.copy io, body, body_len
       body.rewind
-      yield Message.new(exchange, rk, body)
+      io.read_byte # 0x6a nil list tail
+
+      value(io) # garbage?
+      value(io) # true
+
+      case ext
+      when ".rdq"
+        io.read_byte # 0xff
+      end
+
+      yield Message.new(exchange, rk, p, body)
     rescue IO::EOFError
       break
     ensure
       body.clear
+    end
+  end
+
+  private def value(io)
+    type = io.read_byte
+    case type
+    when 0x61
+      io.read_byte # uint8
+    when 0x62 # int32
+     io.read_bytes Int32, IO::ByteFormat::NetworkEndian
+    when 0x64 # short string
+      v = io.read_string(io.read_bytes Int16, IO::ByteFormat::NetworkEndian)
+      v == "undefined" ? nil : v
+    when 0x6a # nil
+      io.skip 2 # 0x61 0x02
+      nil
+    when 0x6d # long string
+      io.read_string(io.read_bytes Int32, IO::ByteFormat::NetworkEndian)
+    else raise "Unknown data type #{type}"
     end
   end
 
