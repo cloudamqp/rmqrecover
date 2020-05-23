9649f84ea01a2403878b825f6ae5ac0a39956da7
Carl Hörberg <carl.hoerberg@gmail.com>
2020-05-23 21:44:49 +0200

    quote variables in build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 shards build --release --production --static
 dir=rmqrecover-$(shards version)-static-$(uname -i)
-rm -rf $dir
-mkdir $dir
-cp bin/rmqrecover README.md LICENSE $dir
-strip $dir/rmqrecover
-tar -zcvf $dir.tar.gz $dir
-rm -rf $dir
+rm -rf "$dir"
+mkdir "$dir"
+cp bin/rmqrecover README.md LICENSE "$dir"
+strip "$dir/rmqrecover"
+tar -zcvf "$dir.tar.gz" "$dir"
+rm -rf "$dir"
