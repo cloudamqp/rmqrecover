cb7990bde3c18de8c65b7c440d116ea4f1e46550
Carl Hörberg <carl@84codes.com>
2021-02-18 00:06:38 +0100

    build-with-docker/podman script

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,6 +1,11 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
-shards build --release --production --static
+docker=$(which podman || which docker)
+$docker build -t rmqrecover .
+id=$($docker create rmqrecover)
+$docker cp "$id":rmqrecover bin/
+$docker rm "$id"
+
 dir=rmqrecover-$(git describe)-static-$(uname -i)
 rm -rf "$dir"
 mkdir "$dir"
